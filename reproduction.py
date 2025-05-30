e56f50dc89c1bba7218d937d20ba22383870331e
meaniebeanie22 <73771297+meaniebeanie22@users.noreply.github.com>
2025-05-30 10:28:58 +0000

    actually make page

--- reproduction.py
+++ reproduction.py
@@ -1,13 +1,19 @@
 from azure.identity import DeviceCodeCredential
 from msgraph import GraphServiceClient
 from msgraph.generated.models.onenote_page import OnenotePage
+from msgraph.generated.users.item.onenote.sections.item.pages.pages_request_builder import (
+    PagesRequestBuilder,
+)
 import asyncio
+from pprint import pprint
 
 AZURE_SETTINGS = {
     "clientId": "6eb5c9aa-3e3b-4e84-aaf2-43e645d31fd7",
     "tenantId": "348841ce-b851-46d7-a6b2-96bba96fea69",
     "graphUserScopes": "User.Read Notes.ReadWrite.All Notes.Create",
 }
+
+SECTION_ID = "1-66e372e8-2950-460a-92cc-d0ee26dccdc7"
 
 class Graph:
     settings: dict
@@ -26,14 +32,32 @@ class Graph:
         self.graph_client = GraphServiceClient(
             self.device_code_credential, graph_scopes
         )
-        return None
 
-    async def make_page(self, contents, title):
+    async def make_page_request_info(self, contents, title):
+        request_config = (
+            PagesRequestBuilder.PagesRequestBuilderPostRequestConfiguration()
+        )
+        request_config.headers.add("Content-Type", "application/xhtml+xml")
+        request_config.headers.add("boundary", "MyPartBoundary")
+
         page = OnenotePage(content=contents, title=title)
         page_post_request = self.graph_client.me.onenote.sections.by_onenote_section_id(
-            "1-66e372e8-2950-460a-92cc-d0ee26dccdc7"
-        ).pages.to_post_request_information(page) # this points to a section in my onenote
+            SECTION_ID
+        ).pages.to_post_request_information(page, request_config) # this points to a section in my onenote
         return page_post_request
+    
+    async def make_page(self, contents, title):
+        request_config = (
+            PagesRequestBuilder.PagesRequestBuilderPostRequestConfiguration()
+        )
+        request_config.headers.add("Content-Type", "application/xhtml+xml")
+        request_config.headers.add("boundary", "MyPartBoundary")
+        
+        page = OnenotePage(content=contents, title=title)
+        posted_page = self.graph_client.me.onenote.sections.by_onenote_section_id(
+            "1-66e372e8-2950-460a-92cc-d0ee26dccdc7"
+        ).pages.post(page, request_config) # this points to a section in my onenote
+        return await posted_page
 
 page_html = """
 <!DOCTYPE html>
@@ -50,8 +74,10 @@ page_html = """
 
 async def main():
     graph: Graph = Graph(AZURE_SETTINGS)
-    make_page_request = await graph.make_page(page_html, "Onenote make page test")
-    print(make_page_request.content)
+    make_page_request = await graph.make_page_request_info(page_html, "Onenote make page test")
+    made_page = await graph.make_page(page_html, "onenote make page test")
+    print(f"Request content:\n{make_page_request.content}\n")
+    pprint(made_page)
 
 if __name__ == "__main__":
     asyncio.run(main())
